d1a28dc9a6eed2c00ff242d428d80507fd04c7a1
kanth2203 <50618691+kanth2203@users.noreply.github.com>
2019-05-14 15:41:08 +0530

    first comment

---
+++ Cpkanthproject/src/kanth1/C1demo.java
@@ -0,0 +1,19 @@
+package kanth1;
+
+public class C1demo {
+public static void main(String[] args) {
+
+int a=10;
+int b=20;
+int c=b*a;
+
+if(c==200)
+{
+System.out.println(c);
+}
+else {
+	System.out.println("flase");
+}
+System.out.println("hello world");
+System.out.println("Chitti 2.o hu huu");
+}}
